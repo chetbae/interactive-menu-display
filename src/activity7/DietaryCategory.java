7309bcc62c0ceb52063e8d7a360052b87e189dc5
max zhang <yixuan.zhang3@mail.mcgill.ca>
2021-01-05 21:03:16 -0800

    add working proj

---
+++ src/activity7/DietaryCategory.java
@@ -0,0 +1,8 @@
+package activity7;
+
+public enum DietaryCategory
+{
+
+	LACTOSE_FREE, VEGETARIAN, VEGAN, GLUTEN_FREE, ORGANIC, KOSHER, HALAL
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,9 @@
+module activity6
+{
+	exports activity7;
+
+	 requires javafx.controls;
+	 requires transitive javafx.graphics;
+	 requires javafx.base;
+
+}
